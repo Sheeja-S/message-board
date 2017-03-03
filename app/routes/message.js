f384d76845da398b22db68ca89ed3b334d201349
Sheeja <sheeja.s18@gmail.com>
2017-03-03 18:36:25 +0800

    new answer js updated

--- app/routes/message.js
+++ app/routes/message.js
@@ -5,6 +5,7 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('message', params.message_id);
   },
+  actions:{
   saveAnswer(params) {
         var newAnswer = this.store.createRecord('answer', params);
         var message = params.message;
@@ -12,7 +13,7 @@ export default Ember.Route.extend({
         newAnswer.save().then(function() {
           return message.save();
         });
-        this.transitionTo('index');
+        this.transitionTo('message', message);
       }
-
+    }
 });
